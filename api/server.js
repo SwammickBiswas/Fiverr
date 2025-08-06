202f4c909d6ebb505823dc75b9ad42d993332c19
SwammickBiswas <22052944@kiit.ac.in>
2025-08-06 20:24:31 +0530

    committed

--- api/server.js
+++ api/server.js
@@ -28,6 +28,10 @@ app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
+app.use("/",(req,res)=>{
+  res.send("API is running");
+})
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/gigs", gigRoute);
