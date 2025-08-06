f54640588b18850eecf386e2f248bf959c0b891f
SwammickBiswas <22052944@kiit.ac.in>
2025-08-06 20:56:11 +0530

    Origin  changed

--- api/server.js
+++ api/server.js
@@ -24,7 +24,9 @@ const connect = async () => {
   }
 };
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+const allowedOrigins = ["http://localhost:5173", "https://fiverr-frontend-neon.vercel.app"];
+
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
